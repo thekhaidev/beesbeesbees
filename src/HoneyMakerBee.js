5838b63bc85865ba6f774cb7d15db46fda520acd
Khai Tran <kqtran917@yahoo.com>
2021-09-27 13:44:39 -0500

    Added implementation for Honeymaker Bee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  new Bee();
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = Bee.prototype.color;
+  this.food = Grub.prototype.food;
+  HoneyMakerBee.prototype.eat = Grub.prototype.eat;
+  this.honeyPot = 0;
+  HoneyMakerBee.prototype.makeHoney = function() {
+    this.honeyPot++;
+  };
+  HoneyMakerBee.prototype.giveHoney = function() {
+    this.honeyPot--;
+  };
 };
 
