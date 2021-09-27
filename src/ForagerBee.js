a05b65dc1ee3f24b5074dbfd9a9537aebd4a5c90
Khai Tran <kqtran917@yahoo.com>
2021-09-27 13:48:28 -0500

    Added implementation for ForageBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  new Bee();
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = Bee.prototype.color;
+  this.food = Grub.prototype.food;
+  ForagerBee.prototype.eat = Grub.prototype.eat;
+  this.canFly = true;
+  this.treasureChest = [];
+  ForagerBee.prototype.forage = function() {
+    this.treasureChest.push('treasure');
+  };
 };
 
