87d64a88b2268d4e9dd36feb58bf09fc3e517450
Khai Tran <kqtran917@yahoo.com>
2021-09-27 13:39:10 -0500

    Added Bee.js implementation

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
 var Bee = function() {
+  new Grub();
+  this.age = 5;
+  this.color = 'yellow';
+  this.food = Grub.prototype.food;
+  Bee.prototype.eat = Grub.prototype.eat;
+  this.job = 'keep on growing';
+
+  _.extend(Bee.prototype, this);
+
 };
 
